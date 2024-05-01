1b7fb757d2f4c88d682ed1d4e6eeaaa0e7b81ccf
josephelias94 <jose.elias.0703@gmail.com>
2024-05-01 16:01:49 -0300

    implement get and set user

---
+++ internals/twitter/twitter.go
@@ -0,0 +1,70 @@
+package twitter
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/josephelias94/tweet-deleter/internals/constants"
+	"github.com/josephelias94/tweet-deleter/internals/models"
+)
+
+type Client struct {
+	Token string
+	User  models.User
+}
+
+func buildUrl(url, wildcard, value string) string {
+	return strings.ReplaceAll(url, wildcard, value)
+}
+
+func parseJson[T any](response []byte, v *T) {
+	if err := json.Unmarshal(response, &v); err != nil {
+		log.Fatalf("twitter: Something went wrong while converting json. Message: \"%v\"", err)
+	}
+}
+
+func (c *Client) makeRequest(method, url string, body io.Reader) ([]byte, error) {
+	client := &http.Client{}
+
+	request, err := http.NewRequest(method, url, body)
+	if err != nil {
+		log.Printf("twitter: Something went wrong while building a request. Message: \"%v\"", url)
+		return nil, err
+	}
+
+	request.Header.Add("Authorization", "Bearer "+c.Token)
+
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Printf("twitter: Something went wrong during a request. Message: \"%v\"", err)
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	resBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("twitter: Something went getting the response. Message: \"%v\"", err)
+		return nil, err
+	}
+
+	return resBody, nil
+}
+
+func (c *Client) SetUser(username string) {
+	url := buildUrl(constants.GET_USER_BY_USERNAME, ":username", username)
+	response, err := c.makeRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return
+	}
+
+	body := models.GetUserResponse{}
+
+	parseJson(response, &body)
+
+	c.User = body.Data
+}
--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"github.com/josephelias94/tweet-deleter/internals/routes"
+	"fmt"
+
+	"github.com/josephelias94/tweet-deleter/internals/envs"
+	"github.com/josephelias94/tweet-deleter/internals/twitter"
 )
 
 func main() {
-	routes.StartServer()
+	envs.Load()
+	// routes.StartServer()
+
+	client := &twitter.Client{
+		Token: envs.GetBearerToken(),
+	}
+
+	client.SetUser("assimfalouojose")
+
+	fmt.Println(client.User)
 }
