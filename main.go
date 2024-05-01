0e3116cbfdd63d749fea8b33e5c02713b2538c2d
josephelias94 <jose.elias.0703@gmail.com>
2024-05-01 16:50:21 -0300

    add twitter execution in routine

--- main.go
+++ main.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 
 	"github.com/josephelias94/tweet-deleter/internals/envs"
+	"github.com/josephelias94/tweet-deleter/internals/routes"
 	"github.com/josephelias94/tweet-deleter/internals/twitter"
 )
 
 func main() {
 	envs.Load()
-	// routes.StartServer()
 
+	go execTwitter()
+
+	routes.StartServer()
+}
+
+func execTwitter() {
 	client := &twitter.Client{
 		Token: envs.GetBearerToken(),
 	}
