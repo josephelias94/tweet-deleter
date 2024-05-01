38ca666f7f4e698d4b91c9a7812de2bae6cff963
josephelias94 <jose.elias.0703@gmail.com>
2024-05-01 15:06:16 -0300

    add url consts

---
+++ internals/constants/url.go
@@ -0,0 +1,7 @@
+package constants
+
+var (
+	DELETE_TWEET         = "https://api.twitter.com/2/tweets/:id"
+	GET_TWEETS_BY_USER   = "https://api.twitter.com/2/users/:id/tweets?max_results=50"
+	GET_USER_BY_USERNAME = "https://api.twitter.com/2/users/by/username/:username"
+)
