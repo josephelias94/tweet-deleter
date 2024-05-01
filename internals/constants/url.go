1f554e704cea698c63106b0eda02db7b65de5bbf
josephelias94 <jose.elias.0703@gmail.com>
2024-05-01 16:53:55 -0300

    remove max_results

--- internals/constants/url.go
+++ internals/constants/url.go
@@ -1,7 +1,10 @@
 package constants
 
+// This consumes monthly cap usage
+// GET_TWEETS_BY_USER   = "https://api.twitter.com/2/users/:id/tweets?max_results=50"
+
 var (
 	DELETE_TWEET         = "https://api.twitter.com/2/tweets/:id"
-	GET_TWEETS_BY_USER   = "https://api.twitter.com/2/users/:id/tweets?max_results=50"
+	GET_TWEETS_BY_USER   = "https://api.twitter.com/2/users/:id/tweets"
 	GET_USER_BY_USERNAME = "https://api.twitter.com/2/users/by/username/:username"
 )
