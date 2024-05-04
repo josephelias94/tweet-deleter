6fa88d21ffe77ac1ae86981871bdfa819d59aa46
josephelias94 <jose.elias.0703@gmail.com>
2024-05-04 10:22:43 -0300

    add api url const

--- internals/constants/url.go
+++ internals/constants/url.go
@@ -4,7 +4,8 @@ package constants
 // GET_TWEETS_BY_USER   = "https://api.twitter.com/2/users/:id/tweets?max_results=50"
 
 var (
-	DELETE_TWEET         = "https://api.twitter.com/2/tweets/:id"
-	GET_TWEETS_BY_USER   = "https://api.twitter.com/2/users/:id/tweets"
-	GET_USER_BY_USERNAME = "https://api.twitter.com/2/users/by/username/:username"
+	API_URL              = "https://api.twitter.com/2"
+	DELETE_TWEET         = API_URL + "/tweets/:id"
+	GET_TWEETS_BY_USER   = API_URL + "/users/:id/tweets"
+	GET_USER_BY_USERNAME = API_URL + "/users/by/username/:username"
 )
