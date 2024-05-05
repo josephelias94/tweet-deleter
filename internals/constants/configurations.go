10fa5e75d9129e3d1350a2780e28cdb68c08607d
josephelias94 <jose.elias.0703@gmail.com>
2024-05-04 23:14:00 -0300

    add scope to refresh token

--- internals/constants/configurations.go
+++ internals/constants/configurations.go
@@ -2,6 +2,6 @@ package constants
 
 var (
 	PORT                  = ":4000"
-	TWITTER_SCOPES        = []string{"tweet.read", "tweet.write", "users.read"}
+	TWITTER_SCOPES        = []string{"tweet.read", "tweet.write", "users.read", "offline.access"}
 	RATE_LIMIT_IN_SECONDS = 960 // 16 minutes
 )
