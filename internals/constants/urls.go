a8c8a487e28636c3c3db53b5878ead1507d5fbd3
josephelias94 <jose.elias.0703@gmail.com>
2024-05-04 23:15:52 -0300

    add comment for possible max_results

--- internals/constants/urls.go
+++ internals/constants/urls.go
@@ -7,6 +7,6 @@ var (
 	OAUTH2_TOKEN = API_URL + "/oauth2/token"
 
 	DELETE_TWEET         = API_URL + "/tweets/:id"
-	GET_TWEETS_BY_USER   = API_URL + "/users/:id/tweets?max_results=50"
+	GET_TWEETS_BY_USER   = API_URL + "/users/:id/tweets?max_results=50" // 40
 	GET_USER_BY_USERNAME = API_URL + "/users/by/username/:username"
 )
